85f682a2ac46cce1d3f54c530fe56ba090c009d6
renaatosalgado <renaato.salgado@gmail.com>
2022-05-18 10:52:04 -0300

    fix: removed old unused code

--- src/pages/SingleRecepie/index.js
+++ src/pages/SingleRecepie/index.js
@@ -10,31 +10,6 @@ import { useParams } from "react-router-dom";
 import api from "../../services/api";
 import useAuth from "../../hooks/useAuth";
 
-const post1 = { name: "eai" };
-const post2 = { name: "ea2" };
-const post3 = { name: "ea3" };
-
-const posts = [post1, post2, post3];
-
-const sidebar = {
-  title: "About",
-  description:
-    "Etiam porta sem malesuada magna mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.",
-  archives: [
-    { title: "March 2020", url: "#" },
-    { title: "February 2020", url: "#" },
-    { title: "January 2020", url: "#" },
-    { title: "November 1999", url: "#" },
-    { title: "October 1999", url: "#" },
-    { title: "September 1999", url: "#" },
-    { title: "August 1999", url: "#" },
-    { title: "July 1999", url: "#" },
-    { title: "June 1999", url: "#" },
-    { title: "May 1999", url: "#" },
-    { title: "April 1999", url: "#" },
-  ],
-};
-
 export default function SingleRecepie() {
   const params = useParams();
   const { recepieId } = params;
